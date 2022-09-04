be75f8700c6c0e9b2d085d4be0c90e40a45d7bdf
Turing Jobs <turing.algeria@gmail.com>
2022-09-04 23:37:20 +0100

    khrza

--- router/index.js
+++ router/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var path = require("path");
 router.get("/*", function (req, res, next) {
   if (!(req.subdomains.length === 1 && req.subdomains[0] === "panel"))
-    res.sendFile(path.resolve("statics", "home.html"));
+    res.sendFile(path.resolve("public", "home.html"));
   else return res.sendFile(path.resolve("public", "panel", "index.html"));
 });
 
