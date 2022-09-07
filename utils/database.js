a1737ce9c48022305be9cc8f1963236ea34680ae
Turing Jobs <turing.algeria@gmail.com>
2022-09-07 17:40:12 +0100

    ronaldo herab formulaire

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,8 @@
 const MONGO_SECRET = process.env.MONGO_SECRET || "";
 
-const MONGO_URI = `mongodb+srv://turingjobs:${MONGO_SECRET}@turing-jobs.xspvfre.mongodb.net/?retryWrites=true&w=majority`;
+//const MONGO_URI = `mongodb+srv://turingjobs:${MONGO_SECRET}@turing-jobs.xspvfre.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://agapornography:${MONGO_SECRET}@serverlessinstanceturin.dpqqc.mongodb.net/?retryWrites=true&w=majority
+`;
 
 module.exports = {
   MONGO_URI: MONGO_URI,
