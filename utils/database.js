2b05c46cf3e3df1529c1cae560175b02b942a25b
Djillali <66275395+Djilllali@users.noreply.github.com>
2023-04-26 14:04:03 +0100

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -3,5 +3,6 @@ const MONGO_SECRET = process.env.MONGO_SECRET || "";
 const MONGO_URI = `mongodb+srv://agapornography:${MONGO_SECRET}@serverlessinstanceturin.dpqqc.mongodb.net/?retryWrites=true&w=majority`;
 
 module.exports = {
+  
   MONGO_URI: MONGO_URI,
 };
